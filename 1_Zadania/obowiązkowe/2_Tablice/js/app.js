e1ce8577a687a65e09c355535a2025c2f0fbcf24
Grzegorz Sobociński <grzegorz.sobocinski@microsoft.wsei.edu.pl>
2019-10-06 09:26:13 +0200

    #3 Finished task 2

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,4 +1,4 @@
-function distFromAverage(array){
+function distFromAverage(array) {
     let sum = 0;
 
     array.forEach(element => {
@@ -10,4 +10,56 @@ function distFromAverage(array){
     return array.map(element => {
         return element - average;
     })
+}
+
+
+function myFavoriteList() {
+    const fruits = ['banana', 'apple', 'strawberry'];
+    console.log(`First: ${fruits[0]}`);
+    console.log(`Last: ${fruits[fruits.length - 1]}`);
+    fruits.forEach(fruit => {
+        console.log(fruit)
+    })
+}
+
+function printTable(array) {
+    array.forEach(element => {
+        console.log(element);
+    })
+}
+
+function multiply(array) {
+    let number = 1;
+    for (let i = 0; i < array.length; i++) {
+        number *= array[i];
+    }
+    return number;
+}
+
+function getEvenAverage(array) {
+    let sum = 0;
+    array = array.filter(element => element % 2 === 0)
+    array.forEach(element => sum += element);   
+    
+    return sum !== 0 ? sum / array.length : null;
+}
+
+function sortArray(array){
+    return array.sort((a,b) => a - b);
+}
+
+function addArrays(firstArray, secondArray){
+    let biggerArray = firstArray,
+        smallerArray = secondArray;
+
+    if (biggerArray.length !== smallerArray.length){
+        biggerArray = firstArray.length > secondArray.length ? firstArray : secondArray;
+        smallerArray = firstArray.length < secondArray.length ? firstArray : secondArray;
+    }
+
+    for(let i = 0; i < biggerArray.length; i++){
+       biggerArray[i] = smallerArray[i] !== undefined ? biggerArray[i] + smallerArray[i] : biggerArray[i]
+    }
+
+    return biggerArray;
 }
